a3b11ffbb3e6c2eed4ef9d50950e72647a42ad04
Manmeet Gill <contact@manmeetgill.com>
2016-09-29 20:05:40 +1000

    Follow naming conventions

--- PyHDDKeepAlive.py
+++ PyHDDKeepAlive.py
@@ -17,108 +17,106 @@ import time
 __author__ = "Manmeet Gill"
 __contact__ = "contact@manmeetgill.com"
 __website__ = "https://manmeetgill.com"
-__name__ = "PyHDDKeepAlive"
+__script__ = "PyHDDKeepAlive"
 __version__ = "v0.1"
 __git__ = "https://github.com/tf2manu994/PyHDDKeepAlive"
 
-# Initiate variables
-sleep = 55
-validArgs = False
-fileName = ".PyHDD"
-
 # Initiate rest of variables depending on OS
 if os.name == "nt":
-    win = True
-    rootDir = "C:\\"  # Used in help flag, as well as in default preferences
-    rootName = "C drive"  # Used in help flag
-    dirSeparator = "\\"
-    example = "D:\\"
+    WINDOWS = True
+    ROOT_DIR = "C:\\"  # Used in help flag, as well as in default preferences
+    ROOT_NAME = "C drive"  # Used in help flag
+    DIR_SEPARATOR = "\\"
+    EXAMPLE = "D:\\"
 else:
-    win = False
-    rootDir = "/"
-    rootName = "root directory"
-    dirSeparator = "/"
-    example = "/mnt/disk1"
-path = rootDir
+    WINDOWS = False
+    ROOT_DIR = "/"
+    ROOT_NAME = "root directory"
+    DIR_SEPARATOR = "/"
+    EXAMPLE = "/mnt/disk1"
 
-
-def complain():
-    print("No  valid arguments were given, assuming to run at %s" % rootDir)
-    print("If you would like help, run with the argument \"--help\"")
-    sys.exit()
+# Initiate variables
+SLEEP = 55
+VALID_ARGS = False
+FILE_NAME = ".PyHDD"
+PATH = ROOT_DIR
 
 
 # Get command line arguments
-for arg in sys.argv[1:]:
+for argument in sys.argv[1:]:
     # Get the first 7 characters of argument and check if they're "--path"
-    if arg[:7] == "--sleep":
+    if argument[:7] == "--sleep":
         try:
             # Ignore the 8th one because it's an equals, we don"t need that
-            sleep = int(arg[8:])
-            validArgs = True
+            SLEEP = int(argument[8:])
+            VALID_ARGS = True
         except ValueError:
-            print('That\s not a valid amount of time.')
+            print('That\'s not a valid amount of time.')
             print('Make sure you wrote --sleep=NUMBER and not --sleep NUMBER?')
             sys.exit()
 
-    elif arg[:6] == "--path":
-        path = arg[7:]
-        validArgs = True
-        if sleep == 55:
+    elif argument[:6] == "--path":
+        PATH = argument[7:]
+        VALID_ARGS = True
+        if SLEEP == 55:
             continue
         else:
             break
-    elif arg == "help" or "\-help" or "\-\-help":
+    elif argument == "help" or "-help" or "--help":
+        print(__script__)
         print("The command line arguments you can use are:")
-        print("--path=<Insert path to write to here> Default is %s" % rootDir)
+        print("--path=<Insert path to write to here> Default is %s" % ROOT_DIR)
         print("--sleep=<Seconds To Sleep>  Default is 55")
-        print("For example, you might do --path=%s --sleep=30" % example)
+        print("For example, you might do --path=%s --sleep=30" % EXAMPLE)
         sys.exit()
-    elif arg == "contact" or "copyright" or "author":
+    elif argument == "contact" or "copyright" or "author":
         print(__author__)
         print(__contact__)
         print(__website__)
-        print(__name__, __version__)
+        print(__script__, __version__)
         print(__git__)
         print("For help, run with the argument \"--help\"")
         sys.exit()
     else:
-        complain()
+        print("No  valid arguments were given.")
+        print("If you would like help, run with the argument \"--help\"")
+        sys.exit()
 
-if not path.endswith(dirSeparator):
-    path += dirSeparator
 
-if path == "C:\\":
-    path = "C:\\temp\\"
+if not PATH.endswith(DIR_SEPARATOR):
+    PATH += DIR_SEPARATOR
 
-file = path + fileName
-if os.path.isfile(file):
+if PATH == "C:\\":
+    PATH = "C:\\temp\\"
+
+FILE = PATH + FILE_NAME
+if os.path.isfile(FILE):
     print("The file already exists.")
     print("To make sure I don\"t delete something important, I have stopped.")
-    print("Please remove the file ", file)
+    print("Please remove the file ", FILE)
 else:
-    print("Running at ", path, "every", sleep, "seconds!")
+    print("Running at ", PATH, "every", SLEEP, "seconds!")
     # Sets what to write
     toWrite = ("%s %s by %s <%s>"
-               % (__name__, __version__, __author__, __contact__))
+               % (__script__, __version__, __author__, __contact__))
 
     while True:
         try:
-            # The w creates the empty file that we can then write to.
-            writer = open(file, "w")
-            writer.write(toWrite)
-            writer.close()
-            os.remove(file)
-            time.sleep(sleep)
+            # The writer creates the empty file that we can then write to.
+            WRITER = open(FILE, "w")
+            WRITER.write(toWrite)
+            WRITER.close()
+            os.remove(FILE)
+            time.sleep(SLEEP)
         except PermissionError:
             print("Oops, we don't have access to that directory.")
             print("Try another directory on the same drive.")
             sys.exit()
         except KeyboardInterrupt:
             print("Stopping...")
-            if os.path.isfile(file):
-                print("Deleting %s" % file)
-                os.remove(file)
+            if os.path.isfile(FILE):
+                print("Deleting %s" % FILE)
+                os.remove(FILE)
                 sys.exit()
             else:
                 print("Done!")
